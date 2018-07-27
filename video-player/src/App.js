5a3a8f6c3be935dc3f31b238efa7f1e9a5d46ebd
joshparkerj <superfakek@gmail.com>
2018-07-26 19:17:27 -0600

    add video picker

--- video-player/src/App.js
+++ video-player/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Video } from './Video';
+import { Menu } from './Menu';
+
+const VIDEOS = {
+  fast: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4',
+  slow: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4',
+  cute: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4',
+  eek: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4'
+};
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+  }
+  
+  chooseVideo(newVideo){
+    this.setState({src: VIDEOS[newVideo]});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Video Player</h1>
+        <Menu chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src}/>
       </div>
     );
   }
